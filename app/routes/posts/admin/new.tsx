88a0c47c731b0f170f80669d0ab946fc8b31e790
Chris Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2022-06-12 19:56:59 -0400

    handle loading state with remix useTransition

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -1,4 +1,4 @@
-import { Form, useActionData } from "@remix-run/react";
+import { Form, useActionData, useTransition } from "@remix-run/react";
 import type { ActionFunction } from "@remix-run/node";
 import { redirect, json } from "@remix-run/node";
 import { createPost } from "~/models/post.server";
@@ -42,6 +42,10 @@ const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`
 
 export default function NewPostRoute() {
   const errors = useActionData() as ActionData;
+
+  const transition = useTransition();
+  const isCreating = Boolean(transition.submission);
+
   return (
     <Form method="post">
       <p>
@@ -79,9 +83,10 @@ export default function NewPostRoute() {
       <p className="text-right">
         <button
           type="submit"
-          className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+          className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          disabled={isCreating}
         >
-          Create Post
+          {isCreating ? "Creating..." : "Create Post"}
         </button>
       </p>
     </Form>
